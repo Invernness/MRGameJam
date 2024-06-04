b2d2e557868b7845236626a0bb6a91352b7ef9e1
Invernness <104453132+Invernness@users.noreply.github.com>
2024-06-04 13:23:12 +0100

    Window Smash
    Windows now smash

--- GameJamProject/Assets/Scripts/BossScript.cs
+++ GameJamProject/Assets/Scripts/BossScript.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class BossScript : MonoBehaviour
 {
+    [SerializeField] GameObject[] windows;
+    [SerializeField] GameObject[] brokenWindows;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +23,28 @@ public class BossScript : MonoBehaviour
     public void Throw(Transform forceDir, float force)
     {
         GetComponent<Rigidbody>().AddForce(transform.forward * force, ForceMode.Impulse);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.tag == "Window1")
+        {
+            windows[0].gameObject.SetActive(false);
+            brokenWindows[0].gameObject.SetActive(true);
+        }
+
+        if (collision.collider.tag == "Window2")
+        {
+            windows[1].gameObject.SetActive(false);
+            brokenWindows[1].gameObject.SetActive(true);
+        }
+
+        if (collision.collider.tag == "Window3")
+        {
+            windows[2].gameObject.SetActive(false);
+            brokenWindows[2].gameObject.SetActive(true);
+        }
+
     }
 
 
